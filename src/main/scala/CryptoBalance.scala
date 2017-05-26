b71493cae40a6402bf678075615d587c2b843a5b
marcelkoopman <marcel.koopman@gmail.com>
2017-05-26 21:03:59 +0200

    Eth account balance toegevoegd

--- src/main/scala/CryptoBalance.scala
+++ src/main/scala/CryptoBalance.scala
@@ -1,15 +1,13 @@
-import CryptoBalance.httpActor
 import HttpActor.{BtcBalance, BtcEUR, EthBalance, EthEUR}
 import akka.actor.ActorSystem
 import akka.pattern.ask
 import akka.util.Timeout
+import spray.json.DefaultJsonProtocol._
 import spray.json._
-import DefaultJsonProtocol._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.Success
 
 /**
   * Created by marcel on 17-4-17.
@@ -22,14 +20,6 @@ object CryptoBalance extends App {
   val httpActor = system.actorOf(HttpActor.props, "httpActor")
 
   implicit val timeout = Timeout(5 seconds)
-
-  case class EthPriceResult(status: String, message: String, result: String)
-
-  case class PriceResult(EUR: BigDecimal)
-
-  implicit val ethPriceResult = jsonFormat3(EthPriceResult)
-  implicit val priceResult = jsonFormat1(PriceResult)
-
   val etherPrice: Future[BigDecimal] = {
     (httpActor ? EthEUR).mapTo[String].map {
       result =>
@@ -38,16 +28,6 @@ object CryptoBalance extends App {
         priceResult.EUR
     }
   }
-
-  val ether: Future[BigDecimal] =
-    (httpActor ? EthBalance("0xbd31fa88f89699ff2eb3d66b449de77e79bb2053")).mapTo[String].map {
-      result =>
-        val json = result.parseJson
-        val priceResult = json.convertTo[EthPriceResult]
-        val wei = priceResult.result.toDouble
-        BigDecimal(wei / 1000000000000000000L)
-    }
-
   val btcPrice: Future[BigDecimal] = {
     (httpActor ? BtcEUR).mapTo[String].map {
       result =>
@@ -57,26 +37,45 @@ object CryptoBalance extends App {
     }
   }
 
-  val btc: Future[BigDecimal] = (httpActor ? BtcBalance("1M693Ay4qWSS3o3mTbxhmBZ2h1dF9cdw48")).map {
+  implicit val ethPriceResult = jsonFormat3(EthPriceResult)
+  implicit val priceResult = jsonFormat1(PriceResult)
+  val result = for {
+    ethPrice <- etherPrice
+    ethBalance <- ether(ethPrice)
+    btcPrice <- btcPrice
+    btcBalance <- btc(btcPrice)
+  } yield (ethBalance, btcBalance)
+
+  def ether(ethPrice: BigDecimal): Future[BigDecimal] =
+    (httpActor ? EthBalance("0xbd31fa88f89699ff2eb3d66b449de77e79bb2053")).mapTo[String].map {
+      result =>
+        val json = result.parseJson
+        val priceResult = json.convertTo[EthPriceResult]
+        val wei = priceResult.result.toDouble
+        val eth = BigDecimal(wei / 1000000000000000000L)
+        println("ETH balance = " + eth)
+        eth * ethPrice
+    }
+
+  def btc(btcPrice: BigDecimal): Future[BigDecimal] = (httpActor ? BtcBalance("1M693Ay4qWSS3o3mTbxhmBZ2h1dF9cdw48")).map {
     result =>
       val Coin = 100000000L
       val satoshi = result.asInstanceOf[String].toDouble
-      BigDecimal(satoshi) / Coin
+      val btc = BigDecimal(satoshi) / Coin
+      println("BTC balance = " + btc)
+      btc * btcPrice
   }
 
-  val result = for {
-    ethPrice <- etherPrice
-    ethBalance <- ether
-    btcPrice <- btcPrice
-    btcBalance <- btc
-  } yield (ethPrice, ethBalance, btcPrice, btcBalance)
+  case class EthPriceResult(status: String, message: String, result: String)
+
+  case class PriceResult(EUR: BigDecimal)
 
   result.map { r =>
-    val euroBalanceEth = r._1 * r._2
-    println("ETH "+r._2+" = "+euroBalanceEth+" EUR")
-    val euroBalanceBtc = r._3 * r._4
-    println("BTC "+r._3+" = "+euroBalanceBtc+" EUR")
-    println("Total "+(euroBalanceEth + euroBalanceEth)+ " EUR")
+    val euroBalanceEth = r._1
+    println("ETH = €" + euroBalanceEth)
+    val euroBalanceBtc = r._2
+    println("BTC = €" + euroBalanceBtc)
+    println("Total = €" + (euroBalanceEth + euroBalanceEth))
   }.recover {
     case e:Exception => e.printStackTrace()
   }
