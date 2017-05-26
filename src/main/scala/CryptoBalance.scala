5d10a24f5b944602ac7e64600ff547ecd9fa5a07
marcelkoopman <marcel.koopman@gmail.com>
2017-05-26 20:16:29 +0200

    Eth account balance toegevoegd

--- src/main/scala/CryptoBalance.scala
+++ src/main/scala/CryptoBalance.scala
@@ -1,14 +1,15 @@
-import HttpActor.{BtcEUR, EthBalance, EthEUR}
+import HttpActor.{BtcBalance, BtcEUR, EthBalance, EthEUR}
 import akka.actor.ActorSystem
 import akka.pattern.ask
 import akka.util.Timeout
-
+import spray.json._
+import DefaultJsonProtocol._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 /**
   * Created by marcel on 17-4-17.
   */
-object CryptoBalance extends App{
+object CryptoBalance extends App {
 
   println("CryptoBalance")
 
@@ -16,12 +17,27 @@ object CryptoBalance extends App{
   val httpActor = system.actorOf(HttpActor.props, "httpActor")
 
   implicit val timeout = Timeout(5 seconds)
-  (httpActor ? EthEUR).map { json => println("eth = "+json) }
-  (httpActor ? BtcEUR).map { json => println("btc = "+json) }
 
+  case class EthPriceResult(status:String, message:String, result:String)
+  implicit val ethPriceResult = jsonFormat3(EthPriceResult)
 
-  (httpActor ? EthBalance("0xbd31fa88f89699ff2eb3d66b449de77e79bb2053")).map { json => println("eth balance = "+json) }
-
+  (httpActor ? EthEUR).map { json => println("ETH = "+json) }
+  (httpActor ? BtcEUR).map { json => println("BTC = "+json) }
+  (httpActor ? EthBalance("0xbd31fa88f89699ff2eb3d66b449de77e79bb2053")).mapTo[String].map {
+    result =>
+      val json = result.parseJson
+      val priceResult = json.convertTo[EthPriceResult]
+      val wei = priceResult.result.toDouble
+      val eth = BigDecimal(wei / 1000000000000000000L)
+      println("eth balance = "+eth+" ETH")
+  }
+  (httpActor ? BtcBalance("1M693Ay4qWSS3o3mTbxhmBZ2h1dF9cdw48")).map {
+    result =>
+      val Coin = 100000000L
+      val satoshi = result.asInstanceOf[String].toDouble
+      val btc = BigDecimal(satoshi) / Coin
+      println("btc balance = "+btc+ " BTC")
+  }
 
   sys.addShutdownHook( system.terminate() )
 
--- src/main/scala/HttpActor.scala
+++ src/main/scala/HttpActor.scala
@@ -1,10 +1,9 @@
-import HttpActor.{BtcEUR, EthBalance, EthEUR}
+import HttpActor.{BtcBalance, BtcEUR, EthBalance, EthEUR}
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -17,6 +16,7 @@ object HttpActor {
   case class EthEUR()
   case class BtcEUR()
   case class EthBalance(address:String)
+  case class BtcBalance(address:String)
   def props:Props = Props[HttpActor]
 }
 
@@ -28,16 +28,22 @@ class HttpActor extends Actor with ActorLogging{
   implicit val materializer = ActorMaterializer()
 
   def receive = {
-    case EthEUR => getJsonResult("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR")
-    case BtcEUR => getJsonResult("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR")
-    case EthBalance(address) => getJsonResult(buildUri(address))
+    case EthEUR => getStringResult("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR")
+    case BtcEUR => getStringResult("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR")
+    case EthBalance(address) => getStringResult(buildEthUri(address))
+    case BtcBalance(address) => getStringResult(buildBtcUri(address))
   }
 
-  private def buildUri(address:String):String = {
+  private def buildEthUri(address:String):String = {
     "https://api.etherscan.io/api?module=account&action=balance&address="+address+"&tag=latest&apikey="+apiKey
   }
 
-  private def getJsonResult(url:String) = {
+  private def buildBtcUri(address: String):String = {
+    "https://blockchain.info/nl/q/addressbalance/"+address
+  }
+
+
+  private def getStringResult(url:String) = {
     val theSender = sender()
     val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url))
 
